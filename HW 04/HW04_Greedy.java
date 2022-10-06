986db33d4539ecc1744eb06f885a078537abb916
Drag-yong <ypark272@wisc.edu>
2022-10-06 01:04:51 -0500

    Update HW04_Greedy.java

--- HW 04/HW04_Greedy.java
+++ HW 04/HW04_Greedy.java
@@ -3,6 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class HW04_Greedy {
@@ -14,19 +16,28 @@ public class HW04_Greedy {
 		for (int i = 0; i < T; i++) {
 			int cacheLength = Integer.parseInt(br.readLine());
 			int[] cache = new int[cacheLength];
+//			LinkedList<Integer> cache = new LinkedList<>();
 			int request = Integer.parseInt(br.readLine());
-			int[] pages = new int[request];
+//			int[] pages = new int[request];
+			ArrayList<Integer> pages = new ArrayList<>(request);
+
+//			for (int j : cache)
+//				j = Integer.MAX_VALUE;
+			for (int j = 0; j < cacheLength; j++)
+				cache[j] = Integer.MAX_VALUE;
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < request; j++) {
-				pages[j] = Integer.parseInt(st.nextToken());
+//				pages[j] = Integer.parseInt(st.nextToken());
+				pages.add(Integer.parseInt(st.nextToken()));
 			}
 
 			int pageFault = 0;
 			for (int j = 0; j < request; j++) {
 				if (!isExistInTheCache(cache, pages, j, cacheLength)) {
 					int index = maxIndex(cache, pages, j, cacheLength, request);
-					cache[index] = pages[j];
+//					cache[index] = pages[j];
+					cache[index] = pages.get(j);
 					pageFault++;
 				}
 			}
@@ -40,9 +51,15 @@ public class HW04_Greedy {
 		bw.close();
 	}
 
-	private static boolean isExistInTheCache(int[] cache, int[] pages, int pageIndex, int cacheLength) {
+//	private static boolean isExistInTheCache(int[] cache, int[] pages, int pageIndex, int cacheLength) {
+//		for (int i = 0; i < cacheLength; i++)
+//			if (cache[i] == pages[pageIndex])
+//				return true;
+//		return false;
+//	}
+	private static boolean isExistInTheCache(int[] cache, ArrayList<Integer> pages, int pageIndex, int cacheLength) {
 		for (int i = 0; i < cacheLength; i++)
-			if (cache[i] == pages[pageIndex])
+			if (cache[i] == pages.get(pageIndex))
 				return true;
 		return false;
 	}
@@ -55,7 +72,37 @@ public class HW04_Greedy {
 	 * @param pageIndex showing the current index of the page
 	 * @return
 	 */
-	private static int maxIndex(int[] cache, int[] pages, int pageIndex, int cacheLength, int pagesLength) {
+//	private static int maxIndex(int[] cache, int[] pages, int pageIndex, int cacheLength, int pagesLength) {
+//		// It tells the length from the future paging
+//		int[] cacheIndex = new int[cacheLength];
+//
+//		for (int i = 0; i < cacheLength; i++) {
+//			if (cacheIndex[i] < pageIndex) {
+//				cacheIndex[i] = Integer.MAX_VALUE; // This shows if there is no future call, it returns the maximum
+//													// value
+//				for (int j = pageIndex; j < pagesLength; j++)
+//					if (cache[i] == pages[j]) {
+//						cacheIndex[i] = j;
+//						break;
+//					}
+//			}
+//		}
+//
+//		int output = 0;
+//		int max = cacheIndex[0];
+//
+//		for (int i = 1; i < cacheLength; i++) {
+//			if (cacheIndex[i] > max) {
+//				max = cacheIndex[i];
+//				output = i;
+//			}
+//		}
+//
+//		return output;
+//	}
+
+	private static int maxIndex(int[] cache, ArrayList<Integer> pages, int pageIndex, int cacheLength,
+			int pagesLength) {
 		// It tells the length from the future paging
 		int[] cacheIndex = new int[cacheLength];
 
@@ -64,7 +111,7 @@ public class HW04_Greedy {
 				cacheIndex[i] = Integer.MAX_VALUE; // This shows if there is no future call, it returns the maximum
 													// value
 				for (int j = pageIndex; j < pagesLength; j++)
-					if (cache[i] == pages[j]) {
+					if (cache[i] == pages.get(j)) {
 						cacheIndex[i] = j;
 						break;
 					}
